bac9aa42e535c26fe522155b138c7d39493002ce
Francesco-Rapetti <rapettif80@yahoo.it>
2024-05-07 16:50:32 +0200

    Add Videogame debug logs

--- adonet-db-videogame/Program.cs
+++ adonet-db-videogame/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace adonet_db_videogame
 {
-    internal class Program
+    public class Program
     {
+        public const string stringaDiConnessione = "Data Source=localhost;Initial Catalog=videogames;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
         static void Main(string[] args)
         {
-            
+            Videogame prova = new("ciao", "questa è una descrizione", 5, DateTime.Parse("12/4/2022"));
+            Console.WriteLine(prova.Id);
+            Console.WriteLine(prova.Name);
+            Console.WriteLine(prova.Overview);
+            Console.WriteLine(prova.SoftwareHouseId);
+            Console.WriteLine(prova.ReleaseDate);
+            Console.WriteLine(prova.CreatedAt);
+            Console.WriteLine(prova.UpdatedAt);
         }
     }
 }
